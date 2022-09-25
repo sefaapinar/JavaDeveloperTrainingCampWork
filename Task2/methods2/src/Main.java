2c1e1d40ede5a0ba27251daee59153ae94f11ddf
sefaapinar <sefanur.pinar@gmail.com>
2022-09-25 20:43:36 +0300

    Task2
    Task2

---
+++ Task2/methods2/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        String mesaj = "Come to Galatasaray!";
+
+    }
+
+    public static void ekle(){
+        System.out.println("Eklendi!");
+    }
+    public static void  sil(){
+        System.out.println("Silindi!");
+    }
+    public static void guncelle(){
+        System.out.println("Güncellendi!");
+    }
+
+    //void operasyon bir şey yapmasını istediğimiz operasyonlardır.
+
+    public static int topla(){
+        return 5;
+    }
+
+
+}
