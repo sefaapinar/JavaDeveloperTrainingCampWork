58f8008fcc82bb64e803bdb06baeb40ae95bd8c6
sefaapinar <sefanur.pinar@gmail.com>
2022-09-25 21:28:13 +0300

    Task2
    Task2 - Classes

---
+++ Task2/classes/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //classlar referans tiplerdir.
+        CustomerManager customerManager = new CustomerManager();
+        customerManager.Add();
+        customerManager.Update();
+        customerManager.Remove();
+
+    }
+}
--- Task2/methods2/src/Main.java
+++ Task2/methods2/src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         String mesaj = "Come to Galatasaray!";
-
+        String yeniMesaj = sehirVer();
+        System.out.println(yeniMesaj);
+        int sayi = topla(7,2);
+        System.out.println(sayi);
+        int toplam = topla2(2,3,4,5,6);
+        System.out.println(toplam);
     }
 
     public static void ekle(){
@@ -16,8 +21,19 @@ public class Main {
 
     //void operasyon bir şey yapmasını istediğimiz operasyonlardır.
 
-    public static int topla(){
-        return 5;
+    public static int topla(int say1,int say2){
+        return say1+say2;
+    }
+    public static String sehirVer(){
+        return "Kocaeli";
+    }
+
+    public static int topla2(int... sayilar){ //birden fazla integer göndermek istersek int ... bu üç nokta variableArguments olarak adlandırılır.
+        int toplam = 0;
+        for(int sayi:sayilar){
+            toplam +=sayi;
+        }
+        return toplam;
     }
 
 
