128796550a4e186fb3d7541fe4e39a23411b6efe
sefaapinar <sefanur.pinar@gmail.com>
2022-09-27 21:40:02 +0300

    Task2 Overloading
    Task2 Overloading

---
+++ Task2/methodOverloading/src/DortIslem.java
@@ -0,0 +1,13 @@
+public class DortIslem {
+    public int Topla(int sayi1, int sayi2) {
+        return sayi1 + sayi2;
+    }
+
+    public int Topla(int sayi1, int sayi2,int sayi3) {;
+        return sayi1 + sayi2 + sayi3;
+    }
+
+    //imza olayı farklı olduğu için yani bir topla işleminde 2 adet int var
+    //aynı şekilde diğer topla seçeneğinde ise 3 adet olduğu için sorun olmuyor aynı isimde kullanılabilir.
+
+}
