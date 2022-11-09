77ed2f3b68b9f8897be0fd717ef737552d42ceac
sefaapinar <77726883+sefaapinar@users.noreply.github.com>
2022-11-09 22:22:53 +0300

    ProgrammingController

--- kodlama_io_Devs/src/main/java/com/example/kodlama_io_devs/webAPI/ProgrammingLanguageController.java
+++ kodlama_io_Devs/src/main/java/com/example/kodlama_io_devs/webAPI/ProgrammingLanguageController.java
@@ -1,5 +1,59 @@
 package com.example.kodlama_io_devs.webAPI;
 
+import com.example.kodlama_io_devs.business.abstracts.ProgrammingLanguageService;
+import com.example.kodlama_io_devs.business.requests.programmingLanguages.CreateProgrammingLanguageRequest;
+import com.example.kodlama_io_devs.business.requests.programmingLanguages.DeleteProgrammingLanguageRequest;
+import com.example.kodlama_io_devs.business.requests.programmingLanguages.FindByIdProgrammingLanguageRequest;
+import com.example.kodlama_io_devs.business.requests.programmingLanguages.UpdateProgrammingLanguageRequest;
+import com.example.kodlama_io_devs.business.responses.programmingLanguages.GetAllProgrammingLanguagesResponse;
+import com.example.kodlama_io_devs.business.responses.programmingLanguages.GetAllProgrammingLanguagesWithSubTechnologiesResponse;
+import com.example.kodlama_io_devs.business.responses.programmingLanguages.GetByIdProgrammingLanguageResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/programmingLanguages")
 public class ProgrammingLanguageController {
-    /test/
+
+    private ProgrammingLanguageService languageService;
+
+    @Autowired
+    public ProgrammingLanguageController(ProgrammingLanguageService programmingLanguageService) {
+            super();
+        this.languageService = languageService;
+    }
+
+    @GetMapping("/getall")
+    public List<GetAllProgrammingLanguagesResponse> getAllProgrammingLanguagesResponses(){
+       return languageService.getAll();
+    }
+    @PostMapping("/add")
+    public void add(CreateProgrammingLanguageRequest languageRequest){
+        languageService.add(languageRequest);
+    }
+    @GetMapping("/getbyid")
+    public GetByIdProgrammingLanguageResponse getByIdProgrammingLanguageResponse(FindByIdProgrammingLanguageRequest byIdProgrammingLanguageRequest){
+        return languageService.getById(byIdProgrammingLanguageRequest);
+    }
+    @DeleteMapping("/delete")
+    public void delete(DeleteProgrammingLanguageRequest deleteProgrammingLanguageRequest){
+        languageService.delete(deleteProgrammingLanguageRequest);
+    }
+
+    @PutMapping("/update")
+    public void update(UpdateProgrammingLanguageRequest updateProgrammingLanguageRequest){
+        languageService.update(updateProgrammingLanguageRequest);
+    }
+
+    @GetMapping("/getAllWithSubTechnologies")
+    public List<GetAllProgrammingLanguagesWithSubTechnologiesResponse> getAllWithSubTechnologies(){
+        return languageService.getAllWithSubTechnologies();
+    }
+
+
+
+
+
 }
--- kodlama_io_Devs/src/main/java/com/example/kodlama_io_devs/webAPI/SubTechnologyController.java
+++ kodlama_io_Devs/src/main/java/com/example/kodlama_io_devs/webAPI/SubTechnologyController.java
@@ -1,4 +1,21 @@
 package com.example.kodlama_io_devs.webAPI;
 
+import com.example.kodlama_io_devs.business.abstracts.SubTechnologyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping
+@RestController
 public class SubTechnologyController {
+
+    SubTechnologyService technologyService;
+
+    @Autowired
+    public SubTechnologyController(SubTechnologyService technologyService) {
+        super();
+        this.technologyService = technologyService;
+    }
+
+
 }
